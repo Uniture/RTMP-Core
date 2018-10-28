155ba2d834b564cbfd8a4974c7fe0feb314ff40d
AlexeyArno <ledssssa@gmail.com>
2018-10-28 21:44:02 +0300

    Init

---
+++ examples/rtmp_publish/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/strengine/core/av/pktque"
+	"github.com/strengine/core/format"
+	"github.com/strengine/core/av/avutil"
+	"github.com/strengine/core/format/rtmp"
+)
+
+func init() {
+	format.RegisterAll()
+}
+
+// as same as: ffmpeg -re -i projectindex.flv -c copy -f flv rtmp://localhost:1936/app/publish
+
+func main() {
+	file, _ := avutil.Open("projectindex.flv")
+	conn, _ := rtmp.Dial("rtmp://localhost:1936/app/publish")
+	// conn, _ := avutil.Create("rtmp://localhost:1936/app/publish")
+
+	demuxer := &pktque.FilterDemuxer{Demuxer: file, Filter: &pktque.Walltime{}}
+	avutil.CopyFile(conn, demuxer)
+
+	file.Close()
+	conn.Close()
+}
+
---
+++ examples/transcode/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/strengine/core/av"
+	"github.com/strengine/core/av/avutil"
+	"github.com/strengine/core/av/transcode"
+	"github.com/strengine/core/cgo/ffmpeg"
+	"github.com/strengine/core/format"
+)
+
+// need ffmpeg with libfdkaac installed
+
+func init() {
+	format.RegisterAll()
+}
+
+func main() {
+	infile, _ := avutil.Open("speex.flv")
+
+	findcodec := func(stream av.AudioCodecData, i int) (need bool, dec av.AudioDecoder, enc av.AudioEncoder, err error) {
+		need = true
+		dec, _ = ffmpeg.NewAudioDecoder(stream)
+		enc, _ = ffmpeg.NewAudioEncoderByName("libfdk_aac")
+		enc.SetSampleRate(stream.SampleRate())
+		enc.SetChannelLayout(av.CH_STEREO)
+		enc.SetBitrate(12000)
+		enc.SetOption("profile", "HE-AACv2")
+		return
+	}
+
+	trans := &transcode.Demuxer{
+		Options: transcode.Options{
+			FindAudioDecoderEncoder: findcodec,
+		},
+		Demuxer: infile,
+	}
+
+	outfile, _ := avutil.Create("out.ts")
+	avutil.CopyFile(outfile, trans)
+
+	outfile.Close()
+	infile.Close()
+	trans.Close()
+}
